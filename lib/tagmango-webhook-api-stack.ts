a70d4e35e0ffe66b7a66a0ed45a40b1b0bd977ef
Pratik Sah <shivam3448@gmail.com>
2024-11-30 10:46:12 +0530

    Updated stack code to use SNS instead of extra AWS Lambda

--- lib/tagmango-webhook-api-stack.ts
+++ lib/tagmango-webhook-api-stack.ts
@@ -1,8 +1,10 @@
 import * as path from "path";
 import * as cdk from "aws-cdk-lib";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as sns from "aws-cdk-lib/aws-sns";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as subscriptions from "aws-cdk-lib/aws-sns-subscriptions";
 import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
 import { Construct } from "constructs";
 import * as dotenv from "dotenv";
@@ -17,34 +19,68 @@ export class TagmangoWebhookApiStack extends cdk.Stack {
       visibilityTimeout: cdk.Duration.seconds(300),
     });
 
-    const webhookReceiverLambda = new lambda.Function(
-      this,
-      "WebhookReceiverLambda",
-      {
-        runtime: lambda.Runtime.NODEJS_20_X,
-        handler: "index.handler",
-        code: lambda.Code.fromAsset(
-          path.join(__dirname, "lambda/webhook-receiver")
-        ),
-        environment: {
-          QUEUE_URL: webhookQueue.queueUrl,
-        },
-      }
-    );
+    const webhookTopic = new sns.Topic(this, "WebhookTopic", {
+      topicName: "webhook-topic",
+    });
 
-    webhookQueue.grantSendMessages(webhookReceiverLambda);
+    webhookTopic.addSubscription(
+      new subscriptions.SqsSubscription(webhookQueue)
+    );
 
     const api = new apigateway.RestApi(this, "TagmangoWebhookApi", {
       restApiName: "Tagmango Webhook API",
       description: "API for receiving webhook payloads from Tagmango",
     });
 
-    const webhookIntegration = new apigateway.LambdaIntegration(
-      webhookReceiverLambda
-    );
+    const webhookIntegration = new apigateway.AwsIntegration({
+      service: "sns",
+      action: "Publish",
+      integrationHttpMethod: "POST",
+      options: {
+        credentialsRole: new cdk.aws_iam.Role(this, "ApiGatewayToSNSRole", {
+          assumedBy: new cdk.aws_iam.ServicePrincipal(
+            "apigateway.amazonaws.com"
+          ),
+          inlinePolicies: {
+            AllowSNSPublish: new cdk.aws_iam.PolicyDocument({
+              statements: [
+                new cdk.aws_iam.PolicyStatement({
+                  actions: ["sns:Publish"],
+                  resources: [webhookTopic.topicArn],
+                }),
+              ],
+            }),
+          },
+        }),
+        requestParameters: {
+          "integration.request.header.Content-Type":
+            "'application/x-www-form-urlencoded'",
+        },
+        requestTemplates: {
+          "application/json": `Action=Publish&TopicArn=$util.urlEncode('${webhookTopic.topicArn}')&Message=$util.urlEncode($input.body)`,
+        },
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseTemplates: {
+              "application/json": '{"message": "Message sent to SNS"}',
+            },
+          },
+        ],
+      },
+    });
 
     const webhookResource = api.root.addResource("webhook");
-    webhookResource.addMethod("POST", webhookIntegration);
+    webhookResource.addMethod("POST", webhookIntegration, {
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseModels: {
+            "application/json": apigateway.Model.EMPTY_MODEL,
+          },
+        },
+      ],
+    });
 
     const sqsConsumerLambda = new lambda.Function(this, "SQSConsumerLambda", {
       runtime: lambda.Runtime.NODEJS_20_X,
@@ -66,6 +102,9 @@ export class TagmangoWebhookApiStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, "WebhookApiEndpoint", {
       value: api.url + "webhook",
+    });
+    new cdk.CfnOutput(this, "SNSTopicArn", {
+      value: webhookTopic.topicArn,
     });
     new cdk.CfnOutput(this, "SQSQueueUrl", {
       value: webhookQueue.queueUrl,
